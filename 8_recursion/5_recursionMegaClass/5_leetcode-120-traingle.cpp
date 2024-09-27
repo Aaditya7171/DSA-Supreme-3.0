14c0beb215a293f665473e9b052856e407c5a14f
Aaditya7171 <risingaditya91@gmail.com>
2024-09-27 22:47:52 +0530

    updated

--- 8_recursion/5_recursionMegaClass/5_leetcode-120-traingle.cpp
+++ 8_recursion/5_recursionMegaClass/5_leetcode-120-traingle.cpp
@@ -1,5 +1,6 @@
 #include<iostream>
 #include<vector>
+#include<climits>
 
 using namespace std;
 
@@ -7,15 +8,29 @@ using namespace std;
 
 class Solution {
 public:
-    int solve(vector<vector<int>>& triangle, int row, int col){
-        if(row == triangle.size() - 1) return triangle[row][col];
-        
-        int down = triangle[row][col] + solve(triangle, row + 1, col);
-        int downPlusOne = triangle[row][col] + solve(triangle, row + 1, col + 1);
+    // int solve(vector<vector<int>>& triangle, int row, int col){
+    //     if(row == triangle.size()) return 0;
+    //     //if(row == triangle.size() - 1) return triangle[row][col];
 
-        return min(down, downPlusOne);
+    //     int down = triangle[row][col] + solve(triangle, row + 1, col);
+    //     int downPlusOne = triangle[row][col] + solve(triangle, row + 1, col + 1);
+
+    //     return min(down, downPlusOne);
+    // }
+
+    void solve(vector<vector<int>>& triangle, int row, int col, int sum, int &ans){
+        if(row == triangle.size()) {
+            ans = min(ans, sum);
+            return;
+        }
+        solve(triangle, row + 1, col, sum + triangle[row][col], ans);
+        solve(triangle, row + 1, col + 1, sum + triangle[row][col], ans);
     }
+
     int minimumTotal(vector<vector<int>>& triangle) {
-        return solve(triangle, 0, 0);
+        // return solve(triangle, 0, 0);
+        int ans = INT_MAX;
+        solve(triangle, 0, 0, 0, ans);
+        return ans;
     }
 };
