509201666697afd8aec38de2face26b15008a4b4
Aaditya7171 <risingaditya91@gmail.com>
2025-10-08 22:14:45 +0530

    Light work ⚙️

---
+++ practiceQuestions/158-leetcode-2300-successfulPairsOfSpellsAndPotions.cpp
@@ -0,0 +1,19 @@
+class Solution
+{
+public:
+    vector<int> successfulPairs(vector<int> &spells, vector<int> &potions, long long success)
+    {
+        sort(potions.begin(), potions.end());
+        int m = potions.size();
+        vector<int> ans;
+        ans.reserve(spells.size());
+
+        for (auto s : spells)
+        {
+            long long need = (success + s - 1) / s;
+            auto it = lower_bound(potions.begin(), potions.end(), need);
+            ans.push_back(m - int(it - potions.begin()));
+        }
+        return ans;
+    }
+};
